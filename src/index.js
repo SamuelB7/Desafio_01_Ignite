8accc51d41529fb6f4b947624771d8c778d6fc11
Samuel Belo <belo.samuel@gmail.com>
2021-03-16 14:35:08 -0300

    modified:   .editorconfig 	modified:   src/index.js 	modified:   src/server.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,65 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.body
+  const userExists = users.some(
+    (user) => user.username === username
+  )
+  if(userExists) {
+    return response.json({error: "User already exists!"})
+  }
+
+  return next()
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', checksExistsUserAccount, (request, response) => {
+  const {name, username} = request.body
+
+  const id = uuidv4()
+  users.push({
+    name,
+    username,
+    id,
+    todo: []
+  })
+  console.log(users)
+  return response.json("User created!")
+});
+
+app.get('/todos', (request, response) => {
+  const {username} = request.headers
+
+  const user = users.find(user => user.username === username)
+  if(!user) return response.json({error: "User not found"})
+
+  return response.json(user.todo)
+});
+
+app.post('/todos', (request, response) => {
+  const {username} = request.headers
+  const user = users.find(user => user.username === username)
+
+});
+
+app.put('/todos/:id', (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.patch('/todos/:id/done', (request, response) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete('/todos/:id', (request, response) => {
   // Complete aqui
 });
 
